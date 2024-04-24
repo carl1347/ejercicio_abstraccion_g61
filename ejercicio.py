975b8c93cc947dee8a03272627b617e16c6dfa8e
Dalton <hello@gmail.com>
2024-04-24 13:43:56 -0500

    ejercicio

---
+++ ejercicio.py
@@ -0,0 +1,31 @@
+from os import name
+
+
+def transformacion(string, metodo):
+    # Completar con lo de los conpañeros
+    pass
+
+if name == "_main_":
+    input_string = input("Ingrese una cadena: ")
+    print("Seleccione un método de transformación:")
+    print("1. Método de transformación 1")
+    print("2. Método de transformación 2")
+    print("3. Método de transformación 3")
+    print("4. Método de transformación 4")
+
+    opcion = input("Ingrese el número correspondiente al método de transformación: ")
+
+
+def Tipo_Transormacion(string, tipo):
+    #completar compañeros
+    pass
+
+if name == "_main_":
+    input_string = input("Ingrese una cadena: ")
+    print("Seleccione un método de transformación:")
+    print("1. Método de transformación 1")
+    print("2. Método de transformación 2")
+    print("3. Método de transformación 3")
+    print("4. Método de transformación 4")
+
+    opcion = input("Ingrese el número correspondiente al método de transformación: ")
